fc64c0bbddd738bc9e73953478870919e2cf2233
pcedik <pcedik@gmail.com>
2014-06-18 12:19:37 +0200

    testi simple formu

--- app/models/aplikace.rb
+++ app/models/aplikace.rb
@@ -2,7 +2,7 @@ class Aplikace < ActiveRecord::Base
 	#attr_accessible :title
 	validates :title,  :presence => true
 	has_many :version, :dependent => :destroy
-	has_many :aplikace_platform
+	has_many :aplikace_platform, :dependent => :destroy
 	has_many :platform, :through => :aplikace_platform
 
 end
